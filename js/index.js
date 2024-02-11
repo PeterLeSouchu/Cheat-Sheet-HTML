b6e85cbb4df179a2f4e9eabb642d8b7dbcc14b9a
PeterLeSouchu <peter22510@gmail.com>
2024-02-11 17:47:08 +0100

    js button

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 const button = document.querySelector("button");
-console.log(button);
+const html = document.querySelector(".container");
+const css = document.querySelector(".container2");
+// console.log(css);
+
+button.addEventListener("click", () => {
+  html.classList.toggle("hide2");
+  css.classList.toggle("hide");
+});
