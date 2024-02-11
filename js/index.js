d4bc58e6820c00d7dc85be98e44f2662a4598402
PeterLeSouchu <peter22510@gmail.com>
2024-02-11 19:18:14 +0100

    button finish

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 const button = document.querySelector("button");
 const html = document.querySelector(".container");
 const css = document.querySelector(".container2");
-// console.log(css);
+const title = document.querySelector(".title-effect");
+// console.log(title);
 
 button.addEventListener("click", () => {
   html.classList.toggle("hide2");
   css.classList.toggle("hide");
+  button.classList.toggle("rotate");
+  title.classList.toggle("hide3");
 });
