bd2e3e4979606ca34ddd9c0204417217279a5230
PeterLeSouchu <peter22510@gmail.com>
2024-02-12 23:44:05 +0100

    v1

--- js/index.js
+++ js/index.js
@@ -2,20 +2,23 @@ const button = document.querySelector("button");
 const html = document.querySelector(".container");
 const css = document.querySelector(".container2");
 const title = document.querySelector(".title-effect");
-const cssSpan = document.querySelector(".css-span")
-const htmlSpan = document.querySelector(".html-span")
+const span = document.querySelector(".change-span")
 
 
 button.addEventListener("click", () => {
   html.classList.toggle("hide2");
   css.classList.toggle("hide");
   button.classList.toggle("rotate");
-  cssSpan.classList.toggle("red");
-  htmlSpan.classList.toggle("html-span");
-  cssSpan.style.transition = "1s";
-  htmlSpan.style.transition = "1s";
-
-
-
-  
+  span.classList.toggle("red");
+  span.style.transition = "1s";
+  change();
 });
+
+ function change() {
+  if (span.textContent === "HTML") {
+    span.innerHTML = `CSS`;
+  }
+  else {
+    span.innerHTML = `HTML`;
+  }
+ }
