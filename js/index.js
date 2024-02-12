1ccec087adb6fd27f21e6520baca1366625d4cbd
PeterLeSouchu <peter22510@gmail.com>
2024-02-12 23:00:28 +0100

    v1 anim title

--- js/index.js
+++ js/index.js
@@ -2,11 +2,20 @@ const button = document.querySelector("button");
 const html = document.querySelector(".container");
 const css = document.querySelector(".container2");
 const title = document.querySelector(".title-effect");
+const cssSpan = document.querySelector(".css-span")
+const htmlSpan = document.querySelector(".html-span")
 
 
 button.addEventListener("click", () => {
   html.classList.toggle("hide2");
   css.classList.toggle("hide");
   button.classList.toggle("rotate");
+  cssSpan.classList.toggle("red");
+  htmlSpan.classList.toggle("html-span");
+  cssSpan.style.transition = "1s";
+  htmlSpan.style.transition = "1s";
+
+
+
   
 });
